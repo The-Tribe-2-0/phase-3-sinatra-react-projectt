33f4cdc6224df42973836832eef7b807096d5f24
Victor <victor.ireri@student.moringaschool.com>
2023-06-08 14:43:22 +0300

    mgrated tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,37 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get '/api/todos' do
+    todos = Todo.all
+    todos.to_json
+  end
 
+  post '/api/todos' do
+    todo = Todo.new(title: params[:title], description: params[:description], category_id: params[:category_id])
+    if todo.save
+      todo.to_json
+    else
+      status 400
+      { error: 'Failed to create todo' }.to_json
+    end
+  end
+
+  patch '/api/todos/:id' do
+    todo = Todo.find(params[:id])
+    if todo.update(title: params[:title], description: params[:description], category_id: params[:category_id])
+      todo.to_json
+    else
+      status 400
+      { error: 'Failed to update todo' }.to_json
+    end
+  end
+  delete '/api/todos/:id' do
+    todo = Todo.find(params[:id])
+    if todo.destroy
+      { success: 'Todo deleted successfully' }.to_json
+    else
+      status 400
+      { error: 'Failed to delete todo' }.to_json
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
+require 'faker'
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+# seeds.rb
+
+
+# Clear existing data
+Category.destroy_all
+Todo.destroy_all
+
+# Generate categories
+5.times do
+  Category.create(name: Faker::Lorem.word)
+end
+
+# Generate todos
+10.times do
+  category = Category.all.sample
+  Todo.create(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    category: category
+  )
+end
+
 
 puts "✅ Done seeding!"
